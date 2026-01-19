35c282c25e0250859b15e27dfab1a6a647886ca1
Odil <is@meteo.uz>
2026-01-19 08:51:29 +0500

    update

--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,11 @@ from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('quiz.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', include('config.urls')),
+    path('quiz/', include('quiz.urls')),  
 )
